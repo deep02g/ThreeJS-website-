b315f0b155a3bc25948f44c83a356857a3143bda
Deep Patel <deepdisha2002@gmail.com>
2025-08-13 19:34:03 -0400

    refactor: extract shared and demo utilities

---
+++ src/core/utils.js
@@ -0,0 +1,15 @@
+export function getMousePos(e) {
+  const x = e.changedTouches ? e.changedTouches[0].clientX : e.clientX;
+  const y = e.changedTouches ? e.changedTouches[0].clientY : e.clientY;
+  const target = e.target;
+
+  return {
+    x,
+    y,
+    target,
+  };
+}
+
+export function mix(a, b, t) {
+  return a * (1 - t) + b * t;
+}
